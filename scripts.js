11d7a3e75846409ca284119bc77f8527396e71e4
Dubymar Tollinchi <dubymarjtr@gmail.com>
2021-04-09 14:19:35 -0500

    listen for a change and update font color and font size

--- scripts.js
+++ scripts.js
@@ -1,7 +1,20 @@
 const p = document.querySelector("p");
+const root = document.querySelector(":root");
 
 document
   .querySelector('input[type="text"]')
   .addEventListener("keyup", (event) => {
     p.innerText = event.target.value;
+  });
+
+document
+  .querySelector('input[type="range"]')
+  .addEventListener("input", (event) => {
+    root.style.setProperty("--font-size", `${event.target.value}px`);
+  });
+
+document
+  .querySelector('input[type="color"]')
+  .addEventListener("input", (event) => {
+    root.style.setProperty("--font-color", `${event.target.value}`);
   });
